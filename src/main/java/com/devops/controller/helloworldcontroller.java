54487493656c344a766e5d38fac81e4d3ce4a9fd
lakshmeprabha <34679995+lakshmeprabha@users.noreply.github.com>
2024-08-03 22:16:20 +0530

    Add files via upload
    Initial commit

---
+++ src/main/java/com/devops/controller/helloworldcontroller.java
@@ -0,0 +1,13 @@
+package com.devops.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class helloworldcontroller   
+{  
+@RequestMapping("/")  
+public String hello()   
+{  
+return "Hello World";  
+}  
+}  
